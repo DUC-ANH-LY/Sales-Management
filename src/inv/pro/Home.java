61930946247160dafe09cb2925d5d5d7637aa624
ducanhly <namobilegame@gmail.com>
2023-08-31 17:39:38 +0700

    change

--- src/inv/pro/Home.java
+++ src/inv/pro/Home.java
@@ -16,7 +16,7 @@ public class Home extends JFrame {
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
-				System.out.println("test");
+				System.out.println("test1");
 				try {
 					Home frame = new Home();
 					frame.setVisible(true);
